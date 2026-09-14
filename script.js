90b1665d666175cb05f9e7b143c0a914cec6cbbd
Guiiz-Hub <aceskream@gmail.com>
2026-09-14 21:15:12 +0200

    Working "Script.js", right link

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const SUPABASE_URL = "sb_publishable_CERIGKSYwpKLsQzyAk5fIw_pJiGo1F9";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1cHF4eWZnYnB2YnBuYWdmbGRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyMzM2ODAsImV4cCI6MjEwNDgwOTY4MH0._LA4boUDX75d6K6KtR_KepqeOpC8v9iupjx7h_v8Xq8";
+const SUPABASE_URL = "https://kupqxyfgbpvbpnagfldi.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_CERIGKSYwpKLsQzyAk5fIw_pJiGo1F9";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 async function chargerBoxes() {
-  const { data, error } = await supabase
+  const { data, error } = await supabaseClient
     .from('box')
     .select('id, numero')
     .order('numero');
