605f7c3d0d44f80c5f0af644b9082a42f19ae945
Guiiz-Hub <aceskream@gmail.com>
2026-09-26 12:52:02 +0200

    Minor update - emoji

--- js/bienvenueContenu.js
+++ js/bienvenueContenu.js
@@ -1,24 +1,24 @@
 export const CONTENU_BIENVENUE = {
-  titre: "Bienvenue au DTEA EVENT 2026 !",
+  titre: "Bienvenue au DTEA EVENT 2026 ⛳️🛫",
   sections: [
     {
-      titre: "Déroulé de la soirée",
+      titre: "Déroulé de la soirée :",
       texte: "Ce soir, c'est practice de golf en équipes ! Vous aurez quelques minutes pour vous échauffer, vous familiariser avec le swing. <br>Ensuite, on commence l'affrontement par un jeu de distance à votre emplacement actuel. <br>Une fois tous les scores enregistrés, un tirage au sort aura lieu pour obtenir des équipes d'un niveau équivalent !"
     },
     {
-      titre: "Consignes de sécurité",
+      titre: "Consignes de sécurité :",
       texte: "Restez toujours derrière la ligne rouge tant qu'un joueur est en train de frapper la balle, objectif zéro blessé !"
     },
     {
-      titre: "Consommations",
-      texte: "Troisième mi-temps constante, attention c'est un marathon, pas un sprint. <br><br>Astuce : N'hésitez pas à payer directement vos premières consommations. Vous serez probablement amener à vous déplacer au cours de la soirée (après le tirage au sort). Les consommations sont liées à un emplacement, pas à une personne.",
+      titre: "Consommations :",
+      texte: "Troisième mi-temps constante, attention c'est un marathon, pas un sprint ! <br><br>Astuce : N'hésitez pas à payer directement vos premières consommations. Vous serez probablement amené à vous déplacer au cours de la soirée (après le tirage au sort). Les consommations sont liées à un emplacement, pas à une personne.",
     },
     {
-      titre: "Organisation",
-      texte: "En cas de problème avec le QR code, le site internet ou le matériel, n'hésitez pas à venir nous voir !"
+      titre: "Organisation :",
+      texte: "En cas de problème avec le QR code, le site internet ou le matériel, n'hésitez pas à venir nous voir. "
     }
 
   ],
   signature: "Louise, Thibault et Guillaume",
-  texteBouton: "J'ai compris, c'est parti !"
+  texteBouton: "🏌🏻LA SUITE 🏌🏼‍♀️"
 };
